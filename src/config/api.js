b49252dad9e027805a1a68a5a181ad50af4c68d7
01strawberri <hanmi200485@gmail.com>
2026-04-07 23:45:08 +0700

    update

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,4 @@
 //const API_URL = "https://gsp26se10-be.fly.dev";
-//const API_URL = "https://bookfet.nport.link";
-const API_URL = "http://localhost:5121";
+const API_URL = "https://bookfet.nport.link";
+//const API_URL = "http://localhost:5121";
 export default API_URL;
